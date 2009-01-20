8b77676bd7285f22e2e87d565c9a978306ac3bd8
Stefan Kopf <s.kopf@xaldon.com>
2009-01-20 16:47:06 +0000

    interface for the Content data type

---
+++ src/org/opendma/api/OdmaContent.java
@@ -0,0 +1,28 @@
+package org.opendma.api;
+
+import java.io.InputStream;
+
+/**
+ * Represents the <code>Content</code> data type in OpenDMA.
+ * 
+ * @author Stefan Kopf, xaldon Technologies GmbH, the OpenDMA architecture board
+ */
+public interface OdmaContent
+{
+    
+    /**
+     * Returns the data as <code>InputStream</code>.<br>
+     * <b>The caller has to close this stream after she/he has finished reading the data.</b>
+     * 
+     * @return the data as <code>InputStream</code>.
+     */
+    public InputStream getStream();
+    
+    /**
+     * Returns the size of the data in octests.
+     * 
+     * @return the size of the data in octests.
+     */
+    public long getSize();
+
+}
