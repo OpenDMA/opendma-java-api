6ab9b17112549cbeed8ef1d26c695ee52b84ebec
Stefan Kopf <s.kopf@xaldon.com>
2009-01-20 12:54:09 +0000

    Interface for the unique object identifier

---
+++ src/org/opendma/api/OdmaId.java
@@ -0,0 +1,31 @@
+package org.opendma.api;
+
+/**
+ * Representation of the <i>unique object identifier<i> that can be used to
+ * identify an <ocde>OdmaObject</code> inside a <code>OdmaRepository</code>.<br>
+ * To globally identify an object you have to use the <code>OdmaGuid</code>.
+ * 
+ * @author Stefan Kopf, xaldon Technologies GmbH, the OpenDMA architecture board
+ */
+public interface OdmaId
+{
+
+    /**
+     * Returns true if the given Object is a <code>OdmaId</code> or an
+     * <code>OdmaGuid</code> identifying the same object.
+     * 
+     * @param obj The object to compare this Id to
+     * 
+     * @return true if and only if the given Object is a <code>OdmaId</code> or an
+     * <code>OdmaGuid</code> identifying the same object.
+     */
+    public boolean equals(Object obj);
+
+    /**
+     * Returns the <code>String</code> Representation of this Id.
+     * 
+     * @return the <code>String</code> Representation of this Id.
+     */
+    public String toString();
+
+}
