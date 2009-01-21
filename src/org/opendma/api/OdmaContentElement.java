61ef5c18db64c6c1200747bd0046e84c3002e026
Stefan Kopf <s.kopf@xaldon.com>
2009-01-21 15:47:22 +0000

    initial empty interfaces

---
+++ src/org/opendma/api/OdmaContentElement.java
@@ -0,0 +1,12 @@
+package org.opendma.api;
+
+/**
+ * The <i>ContentElement</i> specific version of the <code>{@link OdmaObject}</code>
+ * interface that offers short cuts to all defined OpenDMA properties.
+ * 
+ * @author Stefan Kopf, xaldon Technologies GmbH, the OpenDMA architecture board
+ */
+public interface OdmaContentElement extends OdmaObject
+{
+
+}
---
+++ src/org/opendma/api/OdmaFolder.java
@@ -0,0 +1,12 @@
+package org.opendma.api;
+
+/**
+ * The <i>Folder</i> specific version of the <code>{@link OdmaObject}</code>
+ * interface that offers short cuts to all defined OpenDMA properties.
+ * 
+ * @author Stefan Kopf, xaldon Technologies GmbH, the OpenDMA architecture board
+ */
+public interface OdmaFolder extends OdmaObject
+{
+
+}
